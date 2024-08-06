0d5d966fd52a1751ac139dd36255100e8501782f
kevin diaz <kevinds1895@gmail.com>
2024-08-06 21:44:11 +0100

    nuevos cambios en seccion playas

--- src/components/playas.tsx
+++ src/components/playas.tsx
@@ -10,14 +10,23 @@ import {
     Badge,
     Flex, Spacer
 } from '@chakra-ui/react'
-import { useState, useCallback,useMemo } from 'react';
+import { useState, useCallback, useMemo, useEffect } from 'react';
 import { Element, scroller } from 'react-scroll';
 import playasInfo from '../infoWeb/playasInfo';
 
 
 const Playas: React.FC = () => {
     const [currentSection, setCurrentSection] = useState<number>(0);
-    const sectionIds:string[]=useMemo(() =>['section0','section1', 'section2', 'section3', 'section4', 'section5'],[]); // Define los IDs de tus secciones
+    const sectionIds: string[] = useMemo(() => ['section0', 'section1', 'section2', 'section3', 'section4', 'section5'], []); // Define los IDs de tus secciones
+    // const hoverEffect = () => {
+    //     const nameOfBeach = document.getElementsByClassName('nombrePlaya') as HTMLCollectionOf<HTMLDivElement>;
+    //     const imgOfBeach = document.getElementsByClassName("'divImgBeach'") as HTMLCollectionOf<HTMLDivElement>;
+
+    //     for (let i = 0; i < imgOfBeach.length; i++) {
+    //         const element = imgOfBeach[i];
+    //         element.addEventListener('click', () => { nameOfBeach[i].style.display = "block" })
+    //     }
+    // }
 
     // Función para desplazarse a una sección específica
     const scrollToSection = useCallback((index: number) => {
@@ -48,21 +57,47 @@ const Playas: React.FC = () => {
             }
         }
     }, [currentSection, scrollToSection, sectionIds]);
-
-    return (
+        return (
         <Box onWheel={handleWheelEvent}>
-            <Box width={{ base: '60%', sm: '100%', md: '100%', lg: '100%' }} display="flex" marginTop='' alignItems='center' height='100vh' id='section0' justifyContent="center">
-                <Text width={{ base: '60%', sm: '100%', md: '100%', lg: '60%' }}  fontFamily='Oswald' fontWeight='200' fontSize='4xl'>Sumérgete en el paraíso de La Aldea: Playa de La Aldea, Guguy, Tasartico y Tasarte te esperan con el mejor clima del archipiélago canario y puestas de sol inolvidables. 
-                <Text marginTop='50px' align='center' fontWeight='400'>¡Descubre nuestras playas y enamórate del atardecer!</Text></Text>
+            <Box width={{ base: '60%', sm: '100%', md: '100%', lg: '100%' }} display="flex" marginTop='' flexDirection='column' height='100vh' id='section0'>
+                <Box display='flex' justifyContent='space-evenly' gap='1' width='100%'>
+                    <Box className='divImgBeach' width='17%' display='flex' justifyContent='center' alignItems='center'>
+                        <Box className='nombrePlaya' color='white' fontSize='3xl' position='absolute' zIndex='1'>Playa La Aldea</Box>
+                        <Image width={{ base: '15%', sm: '20%', md: '20%', lg: '100%' }} height='100%' className='imgforma' src='..\src\assets\circularPlayaPuerto\la-aldea.jpg'></Image>
+                    </Box>
+                    <Box className='divImgBeach' width='17%' display='flex' justifyContent='center' alignItems='center'>
+                        <Box className='nombrePlaya' color='white' fontSize='3xl' position='absolute' zIndex='1'>Playa Tasarte</Box>
+                        <Image width={{ base: '15%', sm: '20%', md: '20%', lg: '100%' }} height='100%' className='imgforma' src='..\src\assets\circularPlayaPuerto\playa-de-tasarte-en-la-aldea-de-san-nicolas-gran-canaria-4-rincon-restaurante-olivia.webp'></Image>
+                    </Box>
+                    <Box className='divImgBeach' width='17%' display='flex' justifyContent='center' alignItems='center'>
+                        <Box className='nombrePlaya' color='white' fontSize='3xl' position='absolute' zIndex='1'>Playa Tasartico</Box>
+                        <Image width={{ base: '15%', sm: '20%', md: '20%', lg: '100%' }} height='100%' className='imgforma' src='..\src\assets\circularPlayaPuerto\playa-de-tasartico-en-la-aldea-de-san-nicolas-gran-canaria-2-zona-con-tablas-para-tumbarse.webp'></Image>
+                    </Box>
+                    <Box className='divImgBeach' width='17%' display='flex' justifyContent='center' alignItems='center'>
+                        <Box className='nombrePlaya' color='white' fontSize='3xl' position='absolute' zIndex='1'>Playa de Guguy </Box>
+                        <Image width={{ base: '15%', sm: '20%', md: '20%', lg: '100%' }} height='100%' className='imgforma' src='..\src\assets\circularPlayaPuerto\como-llegar-a-playa-de-gui-gui.jpg'></Image>
+                    </Box>
+                    <Box className='divImgBeach' width='17%' display='flex' justifyContent='center' alignItems='center'>
+                        <Box className='nombrePlaya' color='white' fontSize='3xl' position='absolute' zIndex='1'>Playa el Puerto</Box>
+                        <Image width={{ base: '15%', sm: '20%', md: '20%', lg: '100%' }} height='100%' className='imgforma' src='..\src\assets\circularPlayaPuerto\cala-del-puerto-en-la-aldea-de-san-nicolas-gran-canaria-3-zona-norte.webp'></Image>
+                    </Box>
+                </Box>
+                <Box display='flex' justifyContent='center' alignItems='center' height='100vh' width='100%'>
+                    <Text width={{ base: '60%', sm: '100%', md: '100%', lg: '60%' }} fontFamily='Oswald' fontWeight='200' fontSize='4xl'>Sumérgete en el paraíso de La Aldea: Playa de La Aldea, Guguy, Tasartico y Tasarte te esperan con el mejor clima del archipiélago canario y puestas de sol inolvidables.
+                        <Text marginTop='50px' align='center' fontWeight='400'>¡Descubre nuestras playas y enamórate del atardecer!</Text></Text></Box>
             </Box>
             {Object.entries(playasInfo).map((value, index) => {
                 return (
-                    <Box boxSize="xxl" height="100vh" id={`section${index + 1}`} key={value[1].title}>
-                        <Heading  fontFamily='Oswald' marginBottom="30px" marginTop="20px">{value[1].title}</Heading>
-                        <Flex gap="10" alignItems="center">
-                            <Image width="100vh" height="auto" src={value[1].src}></Image>
-                            <Box width="60%">
-                                <Text fontFamily='"Quicksand", sans-serif;' fontSize='xl' width={{ base: '60%', sm: '100%', md: '100%', lg: '60%', xl: "80%" }} dangerouslySetInnerHTML={{ __html: value[1].info }}></Text></Box></Flex></Box>
+                    <Box display='flex' justifyContent='center' boxSize="xxl" height="100vh" id={`section${index + 1}`} key={value[1].title}>
+                        {/* <Heading  fontFamily='Oswald' marginBottom="30px" marginTop="20px">{value[1].title}</Heading> */}
+
+                        <Box display='flex' flexDirection='column' alignItems='center' marginTop='5%' width="50%">
+                            <Heading fontFamily='Oswald' marginBottom="30px" marginTop="20px">{value[1].title}</Heading>
+
+                            <Image width="85%" height="fit-content" src={value[1].src}></Image>
+                        </Box>
+                        <Box width="50%" display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
+                            <Text fontFamily='"Quicksand", sans-serif;' fontSize='xl' width={{ base: '60%', sm: '100%', md: '100%', lg: '60%', xl: "80%" }} dangerouslySetInnerHTML={{ __html: value[1].info }}></Text></Box></Box>
                 )
             }
             )}</Box>
